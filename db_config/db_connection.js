b4c1081a2e35dc1c9f15a2fc85f2604d5218940d
solonkonora <nkwadanora@gmail.com>
2024-07-17 12:34:27 +0100

    fix deployment error

--- db_config/db_connection.js
+++ db_config/db_connection.js
@@ -12,15 +12,15 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
+  // port: process.env.PORT,
   dialect: 'postgres'
 });
 
 //handle connection error
 pool.connect((err) => {
-    if (err) {
-  console.error('error connecting to the database:', err);
-  throw err;
-};
+  if (err) {
+    console.error(err); // Simply display the error as it is
+  }
 
 console.log('Connected to the PostgreSQL database');
 })
